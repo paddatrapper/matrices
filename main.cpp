56cd641a82280002d2cc4705e4eb57a5443f45e1
Kyle Robertze <paddatrapper@gmail.com>
2015-12-21 22:04:45 +0200

    demonstrated multiplication

--- main.cpp
+++ main.cpp
@@ -17,13 +17,24 @@
  */
 int main(int argc, char *argv[])
 {
-	libmatrix::Matrix::matrix_t vector = {
+	libmatrix::Matrix::matrix_t vector_a = {
 		{1.0, 0.0, 0.0},
 		{0.0, 1.0, 0.0},
 		{0.0, 0.0, 1.0}
 	};
-	libmatrix::Matrix a(vector);
-	std::cout << (a + a + a - a);
+	libmatrix::Matrix a(vector_a);
+	libmatrix::Matrix::matrix_t vector_b = {
+		{1.0, 4.0, 1.5},
+		{0.6, 6.0, 8.0},
+		{0.0, 0.0, 1.0}
+	};
+	libmatrix::Matrix b(vector_b);
+	std::cout << "Addition:\n";
+	std::cout << (a + b);
+	std::cout << "Subtraction:\n";
+	std::cout << (a - b);
+	std::cout << "Multiplication by constant:\n";
+	std::cout << (2 * a);
 	return EXIT_SUCCESS;
 }
 
